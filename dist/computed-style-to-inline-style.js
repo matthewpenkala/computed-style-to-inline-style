45486314c144359004004a25b36f80980b95ea0b
math.μ ✯ <96264224+matthewpenkala@users.noreply.github.com>
2023-03-03 08:08:59 -0800

    Update computed-style-to-inline-style.js

--- dist/computed-style-to-inline-style.js
+++ dist/computed-style-to-inline-style.js
@@ -1,23 +1,31 @@
-const _Array$prototype = Array.prototype;
-const each = _Array$prototype.forEach;
-const filter = _Array$prototype.filter;
-
-function computedStyleToInlineStyle(element) {
-  let _context2;
-  const options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-  if (!element) {
-    throw new Error('No element specified.');
-  }
-  if (options.recursive) {
-    let _context;
-    (_context = element.children, each).call(_context, function (child) {
-      computedStyleToInlineStyle(child, options);
+var computedStyleToInlineStyle;
+let each, filter;
+(function () {
+  var _$0 = this;
+  var _$1 = _$0.Array;
+  var _$2 = _$1.prototype;
+  var _$3 = _$2.forEach;
+  var _$4 = _$2.filter;
+  var _1 = function (element) {
+    let _context2;
+    const options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+    if (!element) {
+      throw new Error('No element specified.');
+    }
+    if (options.recursive) {
+      let _context;
+      (_context = element.children, each).call(_context, function (child) {
+        computedStyleToInlineStyle(child, options);
+      });
+    }
+    const computedStyle = getComputedStyle(element);
+    (_context2 = (_context2 = options.properties || computedStyle, filter).call(_context2, function (property) {
+      return !property.startsWith('-webkit');
+    }), each).call(_context2, function (property) {
+      element.style[property] = computedStyle.getPropertyValue(property);
     });
-  }
-  const computedStyle = getComputedStyle(element);
-  (_context2 = (_context2 = options.properties || computedStyle, filter).call(_context2, function (property) {
-    return !property.startsWith('-webkit');
-  }), each).call(_context2, function (property) {
-    element.style[property] = computedStyle.getPropertyValue(property);
-  });
-}
+  };
+  _$0.computedStyleToInlineStyle = _1;
+  each = _$3;
+  filter = _$4;
+}.call(this));

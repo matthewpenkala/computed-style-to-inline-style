ae91d5e3ffe98d079a827ab41ac540b72e765ea5
math.μ ✯ <96264224+matthewpenkala@users.noreply.github.com>
2023-03-03 08:04:33 -0800

    Update computed-style-to-inline-style.js

--- dist/computed-style-to-inline-style.js
+++ dist/computed-style-to-inline-style.js
@@ -1,36 +1,23 @@
-(function (global, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['module'], factory);
-  } else if (typeof exports !== 'undefined') {
-    factory(module);
-  } else {
-    const mod = { exports: {} };
-    factory(mod);
-    global.computedStyleToInlineStyle = mod.exports;
+const _Array$prototype = Array.prototype;
+const each = _Array$prototype.forEach;
+const filter = _Array$prototype.filter;
+
+function computedStyleToInlineStyle(element) {
+  let _context2;
+  const options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  if (!element) {
+    throw new Error('No element specified.');
   }
-}(this, function (module) {
-  'use strict';
-  const _Array$prototype = Array.prototype;
-  const each = _Array$prototype.forEach;
-  const filter = _Array$prototype.filter;
-  function computedStyleToInlineStyle(element) {
-    let _context2;
-    const options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-    if (!element) {
-      throw new Error('No element specified.');
-    }
-    if (options.recursive) {
-      let _context;
-      (_context = element.children, each).call(_context, function (child) {
-        computedStyleToInlineStyle(child, options);
-      });
-    }
-    const computedStyle = getComputedStyle(element);
-    (_context2 = (_context2 = options.properties || computedStyle, filter).call(_context2, function (property) {
-      return !property.startsWith('-webkit');
-    }), each).call(_context2, function (property) {
-      element.style[property] = computedStyle.getPropertyValue(property);
+  if (options.recursive) {
+    let _context;
+    (_context = element.children, each).call(_context, function (child) {
+      computedStyleToInlineStyle(child, options);
     });
   }
-  module.exports = computedStyleToInlineStyle;
-}));
+  const computedStyle = getComputedStyle(element);
+  (_context2 = (_context2 = options.properties || computedStyle, filter).call(_context2, function (property) {
+    return !property.startsWith('-webkit');
+  }), each).call(_context2, function (property) {
+    element.style[property] = computedStyle.getPropertyValue(property);
+  });
+}

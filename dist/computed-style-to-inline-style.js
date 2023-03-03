75965e61ee24cfa98029d37cb8f27012a2296bd6
math.μ ✯ <96264224+matthewpenkala@users.noreply.github.com>
2023-03-03 07:51:44 -0800

    Update computed-style-to-inline-style.js

--- dist/computed-style-to-inline-style.js
+++ dist/computed-style-to-inline-style.js
@@ -4,26 +4,28 @@
   } else if (typeof exports !== 'undefined') {
     factory(module);
   } else {
-    var mod = { exports: {} };
+    const mod = { exports: {} };
     factory(mod);
     global.computedStyleToInlineStyle = mod.exports;
   }
 }(this, function (module) {
   'use strict';
-  var _Array$prototype = Array.prototype, each = _Array$prototype.forEach, filter = _Array$prototype.filter;
+  const _Array$prototype = Array.prototype;
+  const each = _Array$prototype.forEach;
+  const filter = _Array$prototype.filter;
   function computedStyleToInlineStyle(element) {
-    var _context2;
-    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+    let _context2;
+    const options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
     if (!element) {
       throw new Error('No element specified.');
     }
     if (options.recursive) {
-      var _context;
+      let _context;
       (_context = element.children, each).call(_context, function (child) {
         computedStyleToInlineStyle(child, options);
       });
     }
-    var computedStyle = getComputedStyle(element);
+    const computedStyle = getComputedStyle(element);
     (_context2 = (_context2 = options.properties || computedStyle, filter).call(_context2, function (property) {
       return !property.startsWith('-webkit');
     }), each).call(_context2, function (property) {
